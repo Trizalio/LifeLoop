d24fd9efc073a59a2b2f346f9579c41202c3039e
Mike Orlov <m.orlov@technokert.ru>
2020-10-04 21:03:20 +0300

    update

--- scripts/singletones/game_status.gd
+++ scripts/singletones/game_status.gd
@@ -1,16 +1,26 @@
 extends Node
 
 
+
 var no_effect: float = 0
 var small_effect: float = 5
 var medium_effect: float = 10
 var strong_effect: float = 15
+
+var money_per_sheet: float = 1
+var stress_per_mistake: float = 1
 
 var buildings = [
 	BuildingData.new('grocery_store', -small_effect, small_effect, no_effect),
 	BuildingData.new('bar', -medium_effect, -small_effect, medium_effect),
 ]
 var name_to_building = {}
+
+
+var items = [
+	BuildingData.new('bottle', -small_effect, small_effect, no_effect),
+]
+var name_to_items = {}
 
 #const day_to_buildings = {
 #	1: ['bar'],
@@ -33,24 +43,34 @@ class BuildingData:
 		stress_effect = _stress_effect
 		family_effect = _family_effect
 
-#var WorkTimeLabel = preload("res://scenes/work_time.tscn")
-
+#var ResourseBars = preload("res://scenes/bars.tscn")
+signal resources_changed
 var money: float = 50
-var health: float = 50
+var stress: float = 50
 var family: float = 50
 
+var resourse_bars = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in len(buildings):
 		var building: BuildingData = buildings[i]
 		name_to_building[building.title] = building
+	for i in len(items):
+		var item: BuildingData = items[i]
+		name_to_items[item.title] = item
 	
 func start_new_game():
 	money = 50
-	health = 50
+	stress = 50
 	family = 50
 	current_day = 0
+#	var resourse_bars = ResourseBars.instance()
+#	get_node("/root").add_child(resourse_bars)
+	
+func end_game():
+#	get_node("/root").remove_child(resourse_bars)
+	pass
 	
 func try_find(location: String):
 	var node = get_node(location)
@@ -95,20 +115,61 @@ func go_to_office():
 	SceneChanger.goto_scene("res://scenes/city.tscn")
 	yield(SceneChanger, "scene_changed")
 	var city = get_node("/root/city")
-	city.come_to_office()
+	city.come_to_office(['work'])
 	yield(city, "came_to_office")
 	SceneChanger.goto_scene("res://scenes/office.tscn")
 
 func on_building_used(building_name: String):
 	print('building_used: ', building_name)
+	if building_name == 'home':
+		came_home()
+		return
 	var building_data: BuildingData = name_to_building[building_name]
-	print('building_used: ', building_name)
 	
+	modify_resourses(building_data.money_effect, building_data.stress_effect, 
+												building_data.family_effect)
+												
+func used_item(item_name):
+	if item_name == 'bed':
+		go_to_office()
+		return
+	var item_data: BuildingData = name_to_items[item_name]
+	modify_resourses(item_data.money_effect, item_data.stress_effect, 
+												item_data.family_effect)
+
+func modify_resourses(money_value: float, stress_value: float, family_value: float):
+	print('modify_resourses', money_value, stress_value, family_value)
+	money += money_value
+	stress += stress_value
+	family += family_value
+	if money < 0:
+		game_over('no_money')
+	if stress < 0:
+		game_over('died')
+	if family < 0:
+		game_over('lost_family')
+	emit_signal("resources_changed")
+
+func game_over(ending_name: String):
+	pass
+
+func came_home():
+	SceneChanger.goto_scene("res://scenes/home.tscn")
+	yield(SceneChanger, "scene_changed")
+	var home = get_node("/root/home")
+	home.limit_selectable_objects_to(['bed', 'bottle'])
+
+func work_done(result_is_correct):
+	if result_is_correct:
+		modify_resourses(money_per_sheet, 0, 0)
+	else:
+		modify_resourses(0, stress_per_mistake, 0)
+
 func gone_from_office():
 	SceneChanger.goto_scene("res://scenes/city.tscn")
 	yield(SceneChanger, "scene_changed")
 	var city = get_node("/root/city")
 	city.connect("building_used", self, "on_building_used")
 	print('city.connect("building_used", self, "on_building_used")')
-	city.start_at_office(['home',  'bar', 'work'])
+	city.start_at_office(['home',  'bar'])
 	
