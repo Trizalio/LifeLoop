f74b7bdbc82a2bbed8a48bc1982654672a72144c
Mike Orlov <m.orlov@technokert.ru>
2020-10-05 20:16:26 +0300

    bars color change on change

--- scenes/bars.gd
+++ scenes/bars.gd
@@ -11,16 +11,41 @@ extends MarginContainer
 #	money_bar
 #	pass
 
+export (Color) var color_on_decrease = Color(1, 0, 0, 1)
+export (Color) var color_on_increase = Color(0, 1, 0, 1)
+var normal_color = Color(1, 1, 1, 1)
+export (float) var color_keep_factor = 0.01
+
+func merge_colors(base_color: Color, hint_color: Color, hint_power: float) -> Color:
+	return base_color * (1 - hint_power) + hint_color * hint_power
+
+func update_bar_value(bar: ProgressBar, new_value: float):
+	var old_value = bar.value
+	bar.value = new_value
+	var delta = clamp((new_value - old_value) / 10, -1, 1)
+	
+	var color_modulate = bar.modulate
+	if delta > 0:
+		bar.modulate = merge_colors(color_modulate, color_on_increase, delta)
+	elif delta < 0:
+		bar.modulate = merge_colors(color_modulate, color_on_decrease, -delta)
+
 func render():
-	$vbox/bars/money.value = GameStatus.money
-	$vbox/bars/stress.value = GameStatus.stress
-	$vbox/bars/family.value = GameStatus.family
+	update_bar_value($vbox/bars/money, GameStatus.money)
+	update_bar_value($vbox/bars/stress, GameStatus.stress)
+	update_bar_value($vbox/bars/family, GameStatus.family)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	render()
 	GameStatus.connect("resources_changed", self, 'render')
 
+func return_bar_modulate(bar: ProgressBar, delta: float):
+	var keep_factor = pow(color_keep_factor, delta)
+	print('keep_factor:', keep_factor)
+#	keep_factor = 0.5
+	bar.modulate = merge_colors(normal_color, bar.modulate, keep_factor)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	return_bar_modulate($vbox/bars/money, delta)
+	return_bar_modulate($vbox/bars/stress, delta)
+	return_bar_modulate($vbox/bars/family, delta)
