baf91b26ca1d37a5ea503e6081069ae8de7b3386
Mike Orlov <m.orlov@technokert.ru>
2020-10-05 20:46:08 +0300

    more bars fixes

--- scenes/bars.gd
+++ scenes/bars.gd
@@ -8,24 +8,27 @@ export (float) var color_keep_factor = 0.01
 func merge_colors(base_color: Color, hint_color: Color, hint_power: float) -> Color:
 	return base_color * (1 - hint_power) + hint_color * hint_power
 
-func update_bar_value(bar: ProgressBar, new_value: float):
+func update_bar_value(bar: ProgressBar, new_value: float, glow_on_change: bool = true):
 	var old_value = bar.value
 	bar.value = new_value
 	var delta = clamp((new_value - old_value) / 30, -0.5, 0.5)
 	
+	if not glow_on_change:
+		return
+		
 	var color_modulate = bar.modulate
 	if delta > 0:
 		bar.modulate = merge_colors(color_modulate, color_on_increase, 0.5 + delta)
 	elif delta < 0:
 		bar.modulate = merge_colors(color_modulate, color_on_decrease, 0.5 - delta)
 
-func render():
-	update_bar_value($vbox/bars/money, GameStatus.money)
-	update_bar_value($vbox/bars/stress, GameStatus.stress)
-	update_bar_value($vbox/bars/family, GameStatus.family)
+func render(glow_on_change: bool = true):
+	update_bar_value($vbox/bars/money, GameStatus.money, glow_on_change)
+	update_bar_value($vbox/bars/stress, GameStatus.stress, glow_on_change)
+	update_bar_value($vbox/bars/family, GameStatus.family, glow_on_change)
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	render()
+	render(false)
 	GameStatus.connect("resources_changed", self, 'render')
 
 func return_bar_modulate(bar: ProgressBar, delta: float):
